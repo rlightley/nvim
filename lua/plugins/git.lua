9b2b42dfa24d442285fded7bb7c688bf4e55ea18
root <root@LAPTOP-4GO3MNS8.localdomain>
2025-10-27 23:27:46 +0000

    Add lazy git

---
+++ lua/plugins/git.lua
@@ -0,0 +1,20 @@
+return {
+  -- Lazygit integration
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+      { "<leader>gf", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit Current File" },
+    },
+  },
+}
